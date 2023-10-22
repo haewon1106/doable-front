777cabb1f4f51cb946cc6050cc01778f2c5bb56b
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-22 23:16:18 +0900

    feat: 로그인 했을 때 정보 쿠키 저장

--- login/js/login.js
+++ login/js/login.js
@@ -9,14 +9,23 @@ loginButton.onclick = () => {
     let loginRequest = {
         memberId: memberId,
         password: password
-    }
+    };
 
     axios.post('http://localhost:8080/api/auth/login', loginRequest)
     .then(response => {
         console.log('Registration successful:', response.data);
-        window.open(`../main/main.html?id=${response.data.memberNo}`, '_top');
+        window.open(`../main/?id=${response.data.memberNo}`, '_top');
+        setCookie('login', response.data.memberNo, 10);
     })
     .catch(error => {
         console.error('Registration failed:', error);
     });
 };
+
+function setCookie(cookie_name, value, days) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + days);
+  
+    var cookie_value = escape(value) + ((days == null) ? '' : '; expires=' + exdate.toUTCString());
+    document.cookie = cookie_name + '=' + cookie_value;
+  }
