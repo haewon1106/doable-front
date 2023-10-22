797b8e3d8d6ab8ea1e1a4c043830920da2273f8e
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-23 01:39:46 +0900

    fix: 회원가입 로그인 요청 변경

--- login/js/login.js
+++ login/js/login.js
@@ -3,19 +3,19 @@ const pwInput = document.getElementById('pw-input');
 const loginButton = document.getElementsByClassName('login-btn')[0];
 
 loginButton.onclick = () => {
-    let memberId = idInput.value;
+    let userid = idInput.value;
     let password = pwInput.value;
 
     let loginRequest = {
-        memberId: memberId,
+        userid: userid,
         password: password
     };
 
-    axios.post('http://localhost:8080/api/auth/login', loginRequest)
+    axios.post('http://localhost:3000/login', loginRequest)
     .then(response => {
         console.log('Registration successful:', response.data);
-        window.open(`../main/?id=${response.data.memberNo}`, '_top');
-        setCookie('login', response.data.memberNo, 10);
+        // window.open(`../main/?id=${response.data.memberNo}`, '_top');
+        // setCookie('login', response.data.memberNo, 10);
     })
     .catch(error => {
         console.error('Registration failed:', error);
