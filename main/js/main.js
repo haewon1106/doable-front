b3341e988be141124d83852d631677bbc88bf1c0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-29 17:11:25 +0900

    fix: 날짜 불러오기 오류 수정

--- main/js/main.js
+++ main/js/main.js
@@ -9,12 +9,15 @@ function setDate() {
     const now = new Date();
     const today = new Date();
     const currentDay = today.getDay(); 
+    let otherDay = 1;
   
-    // 현재 날짜에서 일요일까지의 날짜 차이를 계산
-    const daysUntilSunday = 1 - currentDay;
+    // 현재 날짜에서 원요일까지의 날짜 차이를 계산
+    const daysUntilMonday = currentDay - 6;
+
+    console.log(daysUntilMonday);
   
     // 현재 날짜에 일요일까지의 날짜 차이를 더하여 일요일의 날짜를 계산
-    today.setDate(today.getDate() + daysUntilSunday);
+    today.setDate(today.getDate() + daysUntilMonday);
     console.log(today);
 
     dateDiv.innerHTML = `${today.getFullYear()}년 ${today.getMonth() + 1}월`;
@@ -25,10 +28,25 @@ function setDate() {
 
             continue;
         }
-        dayList[i].innerHTML = today.getDate() + parseInt(i);
+
+        if  (today.getDate() + parseInt(i) > getLastDayOfMonth()) {
+            dayList[i].innerHTML = otherDay++;
+        } else {
+            dayList[i].innerHTML = today.getDate() + parseInt(i);
+        }
     }
 
     console.log('day', dayList[0].innerHTML)
+}
+
+function getLastDayOfMonth() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1; 
+
+    const lastDay = new Date(year, month, 0);
+
+    return lastDay.getDate(); 
 }
 
 async function setInfo() {
