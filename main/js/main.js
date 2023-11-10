dccbf876744b1f3cb7d648ab11c7ae2f75697f3f
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-10 11:01:19 +0900

    fix: 날짜 수정 다시 함

--- main/js/main.js
+++ main/js/main.js
@@ -13,15 +13,12 @@ function setDate() {
     const now = new Date();
     const today = new Date();
     const currentDay = today.getDay(); 
-    let otherDay = 1;
   
-    // 현재 날짜에서 원요일까지의 날짜 차이를 계산
-    const daysUntilMonday = currentDay - 6;
-
-    console.log(daysUntilMonday);
+    // 현재 날짜에서 일요일까지의 날짜 차이를 계산
+    const daysUntilSunday = 1 - currentDay;
   
     // 현재 날짜에 일요일까지의 날짜 차이를 더하여 일요일의 날짜를 계산
-    today.setDate(today.getDate() + daysUntilMonday);
+    today.setDate(today.getDate() + daysUntilSunday);
     console.log(today);
 
     dateDiv.innerHTML = `${today.getFullYear()}년 ${today.getMonth() + 1}월`;
@@ -32,12 +29,7 @@ function setDate() {
 
             continue;
         }
-
-        if  (today.getDate() + parseInt(i) > getLastDayOfMonth()) {
-            dayList[i].innerHTML = otherDay++;
-        } else {
-            dayList[i].innerHTML = today.getDate() + parseInt(i);
-        }
+        dayList[i].innerHTML = today.getDate() + parseInt(i);
     }
 
     console.log('day', dayList[0].innerHTML)
