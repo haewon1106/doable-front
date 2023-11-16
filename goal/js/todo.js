b4c0fe75ad69eecb0cd1f6d559802c3a896a5d41
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-16 11:39:59 +0900

    feat: 엔터 누르면 카테고리 생성

--- goal/js/todo.js
+++ goal/js/todo.js
@@ -13,6 +13,7 @@
 // 옵션 동적으로 추가하기
 const categorySelBox = [...document.getElementsByClassName('category')];
 const categoryColors = [...document.getElementsByClassName('category-color')];
+const categoryFields = [...document.getElementsByClassName('selboxDirect')];
 
 showUsersCategory();
 
@@ -40,16 +41,53 @@ async function showUsersCategory() {
 
             // 배경색 적용
             categoryColors[i].style.backgroudColor = result.category_color;
-            
-            // 직접 입력 만들기
-            const directOpiton = document.createElement('option');
-            directOpiton.value = 'direct';
-            directOpiton.innerHTML = '카테고리 만들기';
-
             selbox.appendChild(categoryOpiton);
-            selbox.appendChild(directOpiton);
-        })
+        });
 
-        console.log(selbox)
+        // 직접 입력 만들기
+        const directOpiton = document.createElement('option');
+        directOpiton.value = 'direct';
+        directOpiton.innerHTML = '카테고리 만들기';
+        selbox.appendChild(directOpiton);
     });
+}
+
+// 카테고리 이벤트
+categorySelBox.forEach(selbox => {
+
+    // 바뀌었을 때
+    selbox.onchange = () => {
+        console.log(selbox.value);
+        if (selbox.value == 'direct') {
+            selbox.onkeydown = e => {
+                console.log(e);
+            }
+        }
+    }
+});
+
+categoryFields.forEach((field, i) => {
+    field.onkeydown = e => {
+        if (e.keyCode === 13) {
+            const name = field.value;
+            const color = categoryColors[i].style.backgroudColor;
+            createPost(color, name);
+        }
+    }
+});
+
+function createPost(color, name) {
+    const request = {
+        user_no: USER_NO,
+        category_color: color,
+        category_name: name
+    };
+
+    axios.post(`${BASE_URL}/categories`, request)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        })
 }
