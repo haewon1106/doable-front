35962caa0cbdd3cfcc092712070dc978439f2000
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-17 00:32:26 +0900

    feat: 투두 삭제

--- main/js/main.js
+++ main/js/main.js
@@ -52,7 +52,6 @@ async function showUserInfo() {
             welcomeComent.innerHTML = `안녕하세요, ${user.user_name}님`;
             remainingGoals.innerHTML = `오늘의 목표가 0개 남았습니다`; // TODO: 칼럼 추가해서 남은 투두 개수 계산하기
 
-            welcomeComent
         }).catch((err) => {
             console.error(err);
         });
@@ -64,7 +63,6 @@ async function showUsersTodos() {
         .then(response => {
             const dataList = response.data;
             for (let data of dataList) {
-                console.log(data);
                 const goalBox = document.createElement('div');
                 goalBox.className = 'goal-box';
 
@@ -112,12 +110,23 @@ async function showUsersTodos() {
                 todosDiv.appendChild(goalBox);
 
                 modalContent.style.display = 'none';
+
                 dotIcon.onclick = () => {
                     [...document.getElementsByClassName('modalContent')].forEach(e => {
                         e.style.display = 'none';
                     })
                     modalContent.style.display = 'flex';
                 }
+
+                // 투두 삭제
+                deleteDiv.onclick = () => {
+                    axios.delete(`${BASE_URL}/todos/${data.todo_no}`)
+                    .then(response => {
+                        location.reload();
+                    })
+                    .catch(error => console.log(error));
+                }
+
             }
         })
         .catch(err => {
@@ -144,8 +153,6 @@ async function showUsersCategories() {
             keywordContainer.className = 'keyword-container';
             
             for (let data of dataList) {
-                console.log(data);
-
                 const keyword = document.createElement('div');
                 keyword.className = 'keyword';
 
@@ -171,4 +178,3 @@ async function showUsersCategories() {
 
     showUsersTodos();
 }
-
