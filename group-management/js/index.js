b76e923e70e02941594fc90def50a4ecf11439ab
haewon <haewon1106@gmail.com>
2023-11-17 10:47:23 +0900

    Docs : 주석 제거

--- group-management/js/index.js
+++ group-management/js/index.js
@@ -1,27 +1,3 @@
-// $(document).ready(function () {
-//     let longClickTimer;
-//     const myGroup = $('.my-group');
-//     const modal = $('.modal');
-
-//     myGroup.on('mousedown touchstart', handleMouseDown);
-//     myGroup.on('mouseup touchend', handleMouseUp);
-
-//     function handleMouseDown() {
-//         longClickTimer = setTimeout(showModal, 1000); // 1 second
-//     }
-
-//     function handleMouseUp() {
-//         clearTimeout(longClickTimer);
-//     }
-
-//     function showModal() {
-//         modal.css('display', 'flex');
-//     }
-    
-//     function closeModal() {
-//         modal.css('display', 'none');
-//     }    
-// });
 $(function () {
     let clickTimer;
     const myGroup = $('.my-group');
